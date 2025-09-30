c80f886ad6f40335d50c294aa1b6f8bbe9602161
Lenin <leninmariajoseph@gmail.com>
2025-09-30 21:16:49 +0400

    ui

--- send_email.php
+++ send_email.php
@@ -1,22 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $customer_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    
+
     if (!filter_var($customer_email, FILTER_VALIDATE_EMAIL)) {
         echo "Invalid email address.";
         exit;
     }
-    
+
     $to = "kuruier05@gmail.com";
     $subject = "Landing page customer";
     $body = "Customer email: " . $customer_email;
     $headers = "From: kuruier05@gmail.com\r\n";
     $headers .= "Reply-To: " . $customer_email . "\r\n";
-    
+
     if (mail($to, $subject, $body, $headers)) {
         echo "Thank you! Your email has been sent.";
     } else {
-        echo "Sorry, there was an error sending your message.";
+        echo "Sorry, there was an error sending your message. Please check server mail configuration.";
     }
 } else {
     echo "Invalid request.";
